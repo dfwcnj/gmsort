e2ddd1abea8049ccfe9acba4f8b3e1ef2b814d8e
don caldwell <doncaldwell@dons-Laptop.local>
2024-07-25 13:46:46 -0400

    random string lengths types of test variables

--- mergesort_test.go
+++ mergesort_test.go
@@ -30,27 +30,29 @@ func timeslicessort(lns []string) {
 
 func Test_MergeSort(t *testing.T) {
 
-	var l uint = 32
-	ls := []uint{1, 2, 1 << 4, 1 << 8, 1 << 16, 1 << 20, 1 << 24}
+	var l int = 32
+	var r bool = true
 
-	for _, i := range ls {
+	ls := []int{1, 2, 1 << 4, 1 << 8, 1 << 16, 1 << 20, 1 << 24}
 
-		log.Print("testing sort of ", i)
-		lns := randomstrings(i, l)
-		if len(lns) != int(i) {
-			log.Fatal("lns: wanted len ", i, " got ", len(lns))
+	for _, nl := range ls {
+
+		log.Print("testing sort of ", nl)
+		lns := randomstrings(nl, l, r)
+		if len(lns) != int(nl) {
+			log.Fatal("lns: wanted len ", nl, " got ", len(lns))
 		}
 
 		slns := MergeSort(lns)
 
-		if len(slns) != int(i) {
-			log.Fatal("slns: wanted len ", i, " got ", len(slns))
+		if len(slns) != int(nl) {
+			log.Fatal("slns: wanted len ", nl, " got ", len(slns))
 		}
 
 		if !sort.StringsAreSorted(slns) {
-			t.Error("rsort2a failed for size ", i)
+			t.Error("rsort2a failed for size ", nl)
 		} else {
-			log.Print("sort test passed for ", i)
+			log.Print("sort test passed for ", nl)
 		}
 
 		timemymergesort(lns)
