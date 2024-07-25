871b6c9f36866348348d808d1a2a0d1ab4357589
don caldwell <doncaldwell@dons-Laptop.local>
2024-07-25 09:32:52 -0400

    compare with slices.sort

--- mergesort_test.go
+++ mergesort_test.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"slices"
 	"sort"
 	"testing"
+	"time"
 )
+
+func timer(name string) func() {
+	start := time.Now()
+	return func() {
+		fmt.Printf("%s took %v\n", name, time.Since(start))
+	}
+}
+
+func timemymergesort(lns []string) {
+	a := fmt.Sprintf("msort     %d", len(lns))
+	defer timer(a)()
+	MergeSort(lns)
+}
+
+func timeslicessort(lns []string) {
+	a := fmt.Sprintf("slices sort %d", len(lns))
+	defer timer(a)()
+	slices.Sort(lns)
+}
 
 func Test_MergeSort(t *testing.T) {
 
@@ -18,15 +40,20 @@ func Test_MergeSort(t *testing.T) {
 		if len(lns) != int(i) {
 			log.Fatal("lns: wanted len ", i, " got ", len(lns))
 		}
-		rsl := MergeSort(lns)
-		if len(rsl) != int(i) {
-			log.Fatal("rsl: wanted len ", i, " got ", len(rsl))
+
+		slns := MergeSort(lns)
+
+		if len(slns) != int(i) {
+			log.Fatal("slns: wanted len ", i, " got ", len(slns))
 		}
 
-		if !sort.StringsAreSorted(rsl) {
+		if !sort.StringsAreSorted(slns) {
 			t.Error("rsort2a failed for size ", i)
 		} else {
 			log.Print("sort test passed for ", i)
 		}
+
+		timemymergesort(lns)
+		timeslicessort(lns)
 	}
 }
